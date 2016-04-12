6d09b7d177ae78f8064910f6f71c51a9ccf9fc63
javinc <javinczki02@gmail.com>
2016-04-12 17:37:56 +0800

    include hostname

--- serverward.sh
+++ serverward.sh
@@ -1,6 +1,6 @@
 curl https://api.ratatxt.com/outbox \
 -X POST \
 -u "key_Mi5iMzkyMjMwMDFiZTdlYTVlNzM2MTRkZDM0OTYyNDMxNmUwNTRkMDY2:" \
--d '{"text": "'$1' is very loaded right now. please pay attention","origin": "639751303274", "address": "09353708662"}'
+-d '{"text": "'`hostname`' '$1' is very loaded right now. please pay attention","origin": "639751303274", "address": "09353708662"}'
 
 echo $1 `date` >> out.log
